9f13aca0bf63872aeb9c1b4e9d4bbd3aabc38c76
sarah <41605242+sarah-alameddine@users.noreply.github.com>
2020-11-16 14:54:51 +1100

    Add files via upload

---
+++ src/public_html/movies/post-review.php
@@ -0,0 +1,33 @@
+<?php
+error_reporting(E_ALL);
+ini_set('display_errors', '1');
+require_once(__DIR__ . '/../../private_html/classes/MovieBuilder.php');
+require_once(__DIR__ . '/../../private_html/classes/User.php');
+require_once(__DIR__ . '/../../private_html/html-templates/global/session.php');
+
+if (is_logged_in() &&
+    isset($_POST['movie_id']) &&
+    isset($_POST['rating']) &&
+    isset($_POST['comment'])
+    ) {
+
+    echo("in here");
+    $mb = new MovieBuilder();
+    $movie = $mb->get_movie_by_id($_POST['movie_id']);
+    if ($movie === FALSE) {
+        echo("Movie not found");
+        exit();
+    } else if ($movie === DATABASE_ERROR) {
+        echo("An internal error occured");
+        exit();
+    }
+    $review_id = $movie->user_added_review($_SESSION['user']->get_id(), $_POST['rating'], $_POST['comment']);
+
+    header('Location: /movies/title.php?id=' . $movie->get_id());
+    exit();
+
+} else {
+    exit('Internal error');
+}
+
+?>
